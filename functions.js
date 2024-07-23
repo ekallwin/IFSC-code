89db49eda2e99989cc06bbe583bff489a3d52963
Allwin E K <ekallwin@gmail.com>
2024-07-23 19:56:01 +0530

    Add files via upload

--- functions.js
+++ functions.js
@@ -40,3 +40,13 @@ function validateIFSC() {
             });
     }, 500);
 }
+// Disable right-click context menu and show alert
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+    alert("Right click is disabled due to security reasons");
+});
+document.addEventListener("keydown", function (e) {
+    if (e.ctrlKey && (e.key === "I" || e.key === "i" || e.key === "U" || e.key === "u")) {
+        e.preventDefault();
+    }
+});
